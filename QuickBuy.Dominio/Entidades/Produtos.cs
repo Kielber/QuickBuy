4d15e23123716ffda6be89c1160a80236e4cb83d
kielber <kielber@hotmail.com>
2020-03-10 20:28:39 -0300

    Adicionar arquivos de projeto.

---
+++ QuickBuy.Dominio/Entidades/Produtos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Produtos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+
+        public bool ehBoleto 
+        {
+            get {return Id == (int)TipoFormaPagamentoEnum.Boleto;}
+        }
+        public bool ehCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool ehDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+
+        }
+        public bool ehNaoDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+
+    }
+}
+    
+
